6f6bc950df1ccac2aa491903f4f028c0e41be40b
umeda <fredypoul89@gmail.com>
2025-04-24 23:58:20 +0300

    Параметризация Pytest. Практика работы с pytest параметризацией

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -2,7 +2,8 @@ import pytest
 from playwright.sync_api import Page, Playwright, expect
 
 
-def chromium_page(playwright: Playwright) -> Page:
+@pytest.fixture
+def chromium_page(playwright: Playwright) ->Page:
     browser = playwright.chromium.launch(headless=False)
     yield browser.new_page()
     browser.close()
